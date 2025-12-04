465d8ad31b22b1a5136bb6d8385fa7806b3ca175
Herehs <titovdimka07@gmail.com>
2025-12-04 20:51:59 +0300

    initial commit

---
+++ database/database.py
@@ -0,0 +1,81 @@
+import psycopg
+from psycopg import sql
+
+
+class DBController:
+    def __init__(self, dbname, user, password, host, port):
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.conn = None
+
+    def connect(self):
+        try:
+            self.conn = psycopg.connect(
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port,
+                autocommit=False
+            )
+            print("Соединение с базой данных установлено.")
+        except psycopg.Error as e:
+            print(f"Ошибка при подключении к базе данных: {e}")
+            raise
+
+    def disconnect(self):
+        if self.conn:
+            self.conn.close()
+            print("Соединение с базой данных закрыто.")
+
+    def execute_query(self, query, params=None, fetch=False):
+        if not self.conn or self.conn.closed:
+            self.connect()
+
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(query, params)
+
+                if fetch:
+                    return cur.fetchall()
+                else:
+                    self.conn.commit()
+                    return None
+
+        except psycopg.Error as e:
+            self.conn.rollback()
+            print(f"Ошибка выполнения запроса: {e}")
+            raise
+        except Exception as e:
+            print(f"Произошла непредвиденная ошибка: {e}")
+            raise
+
+
+
+
+if __name__ == '__main__':
+    DB_NAME = "shoesdb"
+    DB_USER = "me"
+    DB_PASS = "1488"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
+
+    controller = DBController(DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT)
+
+    try:
+        controller.connect()
+
+        select_query = "SELECT * FROM Users;"
+        results = controller.execute_query(select_query, fetch=True)
+        if results:
+            print("\nПример выборки данных из Orders:")
+            for row in results:
+                print(row)
+
+    except Exception as e:
+        print(f"\nРабота программы завершена с ошибкой: {e}")
+    finally:
+        controller.disconnect()
---
+++ main.py
@@ -0,0 +1,74 @@
+import sys
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QWidget,
+    QPushButton, QVBoxLayout, QLabel,
+    QStackedWidget
+)
+from presentation.login_window import Ui_Login
+from presentation.products import Ui_List_of_products
+
+
+class LoginWindow(QMainWindow, Ui_Login):
+    def __init__(self, manager):
+        super().__init__()
+        self.manager = manager
+        self.setupUi(self)
+
+        def connect_signals(self):
+            self.login_button.clicked.connect(self.handle_login)
+
+        def handle_login(self):
+            self.manager.goto_window("MainWindow")
+            print("--- 🔴 Функция handle_login вызвана! ---")
+
+class ProductListWindow(QMainWindow, Ui_List_of_products):
+    def __init__(self, manager):
+        super().__init__()
+        self.manager = manager
+        self.setupUi(self)
+
+class WindowManager(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Главный Контроллер Окон")
+
+
+        self.stack = QStackedWidget()
+
+        self.windows = {}
+
+        self.setCentralWidget(self.stack)
+
+        """
+        Экран авторизации
+        """
+        login_window_instance = LoginWindow(self)
+        self.stack.addWidget(login_window_instance)
+        self.windows["LoginWindow"] = login_window_instance
+
+        """
+        Экран продуктов
+        """
+        product_window_instance = ProductListWindow(self)
+        self.stack.addWidget(product_window_instance)
+        self.windows["MainWindow"] = product_window_instance
+
+        self.goto_window("LoginWindow")
+
+    def goto_window(self, name):
+        """
+        Метод-контроллер для переключения окон в QStackedWidget.
+        """
+        # Здесь нет ошибки, так как self.windows уже создан в __init__
+        if name in self.windows:
+            widget = self.windows[name]
+            self.stack.setCurrentWidget(widget)
+        else:
+            print(f"Ошибка: Окно с именем '{name}' не найдено.")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    manager = WindowManager()
+    manager.show()
+    sys.exit(app.exec())
