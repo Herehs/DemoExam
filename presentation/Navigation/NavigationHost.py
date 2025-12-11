06f869b172615ed4e9f76d387fff28047e7bf48f
Herehs <titovdimka07@gmail.com>
2025-12-11 18:54:16 +0300

    релиз!!!! но, баг всё ещё с нами, но это не баг, это фича =)

--- presentation/Navigation/NavigationHost.py
+++ presentation/Navigation/NavigationHost.py
@@ -1,3 +1,5 @@
+import random
+import re
 from functools import partial
 
 from PyQt6.QtCore import QDate, Qt
@@ -173,7 +175,7 @@ class List_of_products_screen_UI(QMainWindow, Ui_List_of_products):
             self.refresh_cards()
 
     def goto_orders(self):
-        dlg = OrderListWindow(self.db)
+        dlg = OrderListWindow(self.db, self.user_role)
         dlg.setMinimumWidth(600)
         dlg.connect_signals()
 
@@ -239,7 +241,7 @@ class List_of_products_screen_UI(QMainWindow, Ui_List_of_products):
             return False
 
     # ----- ПЕРЕДАЧА ДАННЫХ ИЗ ОКНА АВТОРИЗАЦИИ В ОКНО ТОВАРОВ -----
-    def set_data(self, username=None, user_role= None):
+    def set_data(self, username=None, user_role= None, user_id=None):
         if username:
             self.username = username
             print("Получено имя пользователя:", username)
@@ -251,17 +253,12 @@ class List_of_products_screen_UI(QMainWindow, Ui_List_of_products):
             role = user_role.strip()
 
             if role == "Гость":
-                self.sort_input.show()
-                self.provider_input.show()
-                self.search_input.show()
-                self.add_product_button.show()
-                self.delete_product_button.show()
-                self.order_button.show()
                 print("gost zaza")
             elif role == "Менеджер":
                 self.sort_input.show()
                 self.provider_input.show()
                 self.search_input.show()
+                self.order_button.show()
                 print("menedjer zaza")
             elif role == "Администратор":
                 self.sort_input.show()
@@ -332,14 +329,19 @@ class List_of_products_screen_UI(QMainWindow, Ui_List_of_products):
             self.scrollLayout.addWidget(card)
 
 class OrderListWindow(QDialog, Ui_Dialog):
-    def __init__(self, db):
+    def __init__(self, db, user_role):
         super().__init__()
         self.db = db
+        self.user_role = user_role
         self.cards = []
         self.selected_card = None
         self.setupUi(self)
         self.refreshOrders()
 
+        if user_role != "Администратор":
+            self.order_button_remove.hide()
+        else:
+            self.order_button_remove.show()
 
     def card_clicked(self, card):
         if self.selected_card and self.selected_card != card:
@@ -351,9 +353,11 @@ class OrderListWindow(QDialog, Ui_Dialog):
     # ----- ОБРАБОТКА ДВОЙНОГО КЛИКА (ВЫЗОВ ОКНА РЕДАКТИРОВАНИЯ) -----
     def card_double_clicked(self, card):
 
-        dlg2 = EditOrderWindow(77, self.db)
+        dlg2 = EditOrderWindow(self.db)
         dlg2.set_data(id=card.id, status=card.status,
-                      address=card.address, order_date=card.order_date, delivery_date=card.delivery_date)
+                      address=card.address, order_date=card.order_date,
+                      delivery_date=card.delivery_date, quantity= card.quantity,
+                      username= card.username, product_id= card.product_id, user_id=card.user_id)
         dlg2.connect_signals()
 
         res = dlg2.exec()
@@ -371,10 +375,12 @@ class OrderListWindow(QDialog, Ui_Dialog):
         self.cards = []
 
         query = """
-                SELECT o.product_id, o.order_id, o.order_status, p.address, o.order_date, o.delivery_date
+                SELECT o.product_id, o.order_id, o.order_status, p.address, o.order_date, o.delivery_date, o.quantity, u.full_name, u.user_id, p.pick_up_point_id
                 FROM orders as o
                 JOIN pick_up_point as p
-                ON o.pick_up_point_id = p.pick_up_point_id;
+                ON o.pick_up_point_id = p.pick_up_point_id
+                JOIN users as u
+                ON o.user_id = u.user_id;
            """
 
         try:
@@ -382,13 +388,17 @@ class OrderListWindow(QDialog, Ui_Dialog):
             if result:
                 for row in result:
                     print(row)
-                    product_id, order_id, status, address, order_date, delivery_date = row
+                    product_id, order_id, status, address, order_date, delivery_date, quantity, username, user_id, pick_up_point_id = row
                     order_card = OrderDataWidget(
                         id= order_id,
                         status= status,
-                        address= address,
+                        address= f"{pick_up_point_id}: {address}",
                         order_date= order_date.strftime("%Y-%m-%d"),
                         delivery_date= delivery_date.strftime("%Y-%m-%d"),
+                        quantity= quantity,
+                        username= username,
+                        product_id= product_id,
+                        user_id= user_id
                     )
                     self.cards.append(order_card)
 
@@ -402,64 +412,110 @@ class OrderListWindow(QDialog, Ui_Dialog):
         for card in self.cards:
             card.clicked.connect(partial(self.card_clicked, card))
             card.doubleClicked.connect(partial(self.card_double_clicked, card))
-            print(f"Создана карточка заказа: {order_card}")
             self.verticalLayout_12.addWidget(card)
 
     def connect_signals(self):
         self.order_button_back.clicked.connect(self.close)
+        self.order_button_add.clicked.connect(self.add_order)
+        self.order_button_remove.clicked.connect(self.remove_order)
 
+    def remove_order(self):
+        self.cards.remove(self.selected_card)
+        self.db.execute_query("DELETE FROM orders WHERE order_id = %s", [self.selected_card.id], fetch=False)
+        self.refreshOrders()
 
+    def add_order(self):
+        dlg2 = AddOrderWindow(self.db)
+        dlg2.connect_signals()
+
+        res = dlg2.exec()
+        if res == QDialog.DialogCode.Accepted:
+            self.refreshOrders()
 
 class AddOrderWindow(QDialog, Ui_order_edit):
-    def __init__(self, user_id, db):
+    def __init__(self, db):
         super().__init__()
         self.setupUi(self)
-        self.user_id = user_id
         self.quantity = 1
         self.db = db
         self.fetch_products()
         self.fetch_pick_up_points()
+        self.fetch_users()
+
+
+        self.id = self.fetch_max_order_id() + 1
+        self.articul_input.setText(str(self.id))
+        self.status = None
+        self.address = None
+        self.quantity = None
+        self.username = None
+        self.order_date = None
+        self.delivery_date_str = None
+        self.product_id = None
+
 
 
     def connect_signals(self):
         self.save_button.clicked.connect(self.save_to_db)
         self.save_button.clicked.connect(self.accept)
+        self.cancel_button.clicked.connect(self.close)
 
     def save_to_db(self):
+
+        user_id = self.client_name_input.currentText().split(":")[0]  # Извлекаем user_id из поля с полным именем
+        pick_up_point_id = self.adres_input.currentText().split(":")[0]  # Извлекаем pick_up_point_id из поля с адресом
+        str = self.product_input.currentText().split(":")[1].strip()
+        product_id = str.split(".")[0].strip()
+        order_status = self.status_input.currentText()  # Статус заказа
+        order_date = self.date_order.date().toString("yyyy-MM-dd")  # Дата заказа
+        delivery_date = self.delivery_date.date().toString("yyyy-MM-dd")  # Дата доставки
+        quantity = self.count_input.text()  # Количество товара
+        order_code = random.randint(100, 999)
+
         query = """
-                    UPDATE orders SET
-                        
-                    WHERE product_id = %s;
-                """
-        params = (
-
-            self.status_input.text(),
-            self.adres_input.currentText(),
-            self.date_order.toPlainText(),
-            self.delivery_date.text(),
-
-            self.articul_input.currentText(),
+            INSERT INTO orders (
+            user_id, 
+            pick_up_point_id, 
+            product_id, 
+            order_status, 
+            order_code,
+            order_date, 
+            delivery_date, 
+            quantity
         )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    """
+
+        params = (
+            user_id,
+            pick_up_point_id,
+            product_id,
+            order_status,
+            order_code,
+            order_date,
+            delivery_date,
+            quantity,
+        )
+
         try:
-            result = self.db.execute_query(query, params, fetch=False)
-            if not result:
-                print("Ошибка добавления.")
+            self.db.execute_query(query, params, fetch=False)
+            print("Заказ успешно добавлен!")
 
         except Exception as e:
-            print(f"Ошибка добавления: {e}")
+            print(f"Ошибка при добавлении заказа: {e}")
 
     def fetch_pick_up_points(self):
         query = """
-                   SELECT DISTINCT address FROM pick_up_point;
+                   SELECT DISTINCT pick_up_point_id, address FROM pick_up_point;
                """
         try:
             result = self.db.execute_query(query, fetch=True)
             self.adres_input.clear()
-
+            self.adres_input.addItem("Не выбран")
             if result:
-                for (address,) in result:
-                    print(address)
-                    self.adres_input.addItem(address)
+                for row in result:
+                    pick_up_point_id, address = row
+                    self.adres_input.addItem(f"{pick_up_point_id}: {address}")
 
             else:
                 print("Ошибка загрузки заказов.")
@@ -473,12 +529,13 @@ class AddOrderWindow(QDialog, Ui_order_edit):
               """
         try:
             result = self.db.execute_query(query, fetch=True)
-            self.adres_input.clear()
+            self.product_input.clear()
+            self.product_input.addItem("Не выбран")
 
             if result:
                 for row in result:
                     product_id, name  = row
-                    print(f"АРТ: {product_id}. {name}")
+                    # print(f"АРТ: {product_id}. {name}")
                     self.product_input.addItem(f"АРТ: {product_id}. {name}")
 
             else:
@@ -487,29 +544,129 @@ class AddOrderWindow(QDialog, Ui_order_edit):
         except Exception as e:
             print(f"Ошибка загрузки заказов: {e}")
 
-    def save_to_db(self):
-        print("save_to_db")
+    def fetch_users(self):
+
+        query = """
+                  SELECT user_id, full_name FROM users;
+              """
+        try:
+            result = self.db.execute_query(query, fetch=True)
+            self.client_name_input.clear()
+            self.client_name_input.addItem("Не выбрано")
+
+            if result:
+                for row in result:
+                    id, full_name = row
+                    self.client_name_input.addItem(f"{id}: {full_name}")
+
+            else:
+                print("Ошибка загрузки заказов.")
+
+        except Exception as e:
+            print(f"Ошибка загрузки заказов: {e}")
+
+    def get_product_name(self, product_id):
+        """
+        Получаем имя продукта по его ID
+        """
+        query = "SELECT name FROM products WHERE product_id = %s"
+        try:
+            result = self.db.execute_query(query, [product_id], fetch=True)
+            if result:
+                return result[0][0]  # Возвращаем имя продукта
+            else:
+                return "Не найдено"  # Если продукт не найден
+        except Exception as e:
+            print(f"Ошибка при получении имени продукта: {e}")
+            return "Ошибка"
+
+    def fetch_max_order_id(self):
+        query = """ SELECT MAX(order_id) FROM orders; """
+        try:
+            result = self.db.execute_query(query, fetch=True)
+            if result:
+                for (id,) in result:
+                    return id
+            else:
+                print("Ошибка загрузки заказов.")
+
+        except Exception as e:
+            print(f"Ошибка загрузки заказов: {e}")
 
 class EditOrderWindow(AddOrderWindow):
-    def __init__(self, user_id, db):
-        super().__init__(user_id, db= db)
+    def __init__(self, db):
+        super().__init__(db= db)
         self.db = db
 
-    def set_data(self, id, status, address, order_date, delivery_date):
+    def set_data(self, id, status, address, username, user_id,quantity, order_date, delivery_date, product_id):
 
         self.id = id
         self.status = status
         self.address = address
+        self.quantity = quantity
+        self.username = username
         self.order_date = order_date
         self.delivery_date_str = delivery_date
+        self.product_id = product_id
+        self.user_id = user_id
 
-
-
+        print(address)
         self.articul_input.setText(f"{self.id}")
         self.status_input.setCurrentText(self.status)
-        self.cancel_button.clicked.connect(self.close)
+        self.adres_input.setCurrentText(self.address)
+        self.client_name_input.setCurrentText(f"{self.user_id}: {self.username}")
+        product_text = f"АРТ: {self.product_id}. {self.get_product_name(self.product_id)}"
+        self.product_input.setCurrentText(product_text)
+
+        self.count_input.setText(f"{self.quantity}")
+
         self.date_order.setDate(QDate.fromString(self.order_date, "yyyy-MM-dd"))
         self.delivery_date.setDate(QDate.fromString(self.delivery_date_str, "yyyy-MM-dd"))
+
+    def save_to_db(self):
+
+        user_id = self.client_name_input.currentText().split(":")[0]  # Извлекаем user_id из поля с полным именем
+        pick_up_point_id = self.adres_input.currentText().split(":")[0]  # Извлекаем pick_up_point_id из поля с адресом
+        str = self.product_input.currentText().split(":")[1].strip()
+        product_id = str.split(".")[0].strip()
+        order_status = self.status_input.currentText()  # Статус заказа
+        order_date = self.date_order.date().toString("yyyy-MM-dd")  # Дата заказа
+        delivery_date = self.delivery_date.date().toString("yyyy-MM-dd")  # Дата доставки
+        quantity = self.count_input.text()  # Количество товара
+        order_code = random.randint(100, 999)
+
+        query = """
+            UPDATE orders SET
+                user_id = %s,
+                pick_up_point_id = %s,
+                product_id = %s,
+                order_status = %s,
+                order_code = %s,
+                order_date = %s,
+                delivery_date = %s,
+                quantity = %s
+            WHERE order_id = %s;
+        """
+
+        params = (
+            user_id,
+            pick_up_point_id,
+            product_id,
+            order_status,
+            order_code,
+            order_date,
+            delivery_date,
+            quantity,
+            self.id
+        )
+
+        try:
+            self.db.execute_query(query, params, fetch=False)
+            print("Заказ успешно добавлен!")
+
+        except Exception as e:
+            print(f"Ошибка при добавлении заказа: {e}")
+
 
 class WindowManager(QMainWindow):
     def __init__(self, db):
